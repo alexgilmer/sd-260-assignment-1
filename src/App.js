7e1d703d4031b88b51aa76872155a5b549cb05bb
Alex Gilmer <alexgilmer@student.mitt.ca>
2021-06-08 12:59:19 -0500

    Explore the API

--- src/App.js
+++ src/App.js
@@ -2,16 +2,38 @@ import React, { Component } from 'react';
 import Header from './components/Header';
 import Movie from './components/Movie';
 
+const baseUrl = 'https://api.themoviedb.org/3/search/movie?';
+const apiKey = '5114f9c20b4f4536879395b4e0499ce9';
+
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      favourites: [],
+    }
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const str = event.target[0].value.trim();
+    if (!str) return;
+
+    this.getData(str)
+    .then(data => console.log(data));
+  }
+
+  getData = async (qString) => {
+    const targetUrl = `${baseUrl}api_key=${apiKey}&query=${qString}`
+    const response = await fetch(targetUrl);
+    const data = await response.json();
+
+    return data;
   }
 
   render() { 
     return (
     <>
-      <Header />
+      <Header handleSubmitCB={this.handleSubmit}/>
       <div className="titleList">
         <div className="title">
           <h1>Movies</h1>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,19 @@ import React from 'react';
 const Header = (props) => {
   return ( 
     <header className="header">
-      <a href="#">
+      <a href="index.html">
         <img
           src="https://fontmeme.com/permalink/190707/fd4735271a0d997cbe19a04408c896fc.png"
           alt="netflix-font"
           border="0"
         />
       </a>
+      <form id="search" className="search" onSubmit={props.handleSubmitCB}>
+        <input
+          type="search"
+          placeholder="Search for a title..."
+        />
+      </form>
     </header>
    );
 }
